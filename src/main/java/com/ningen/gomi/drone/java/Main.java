5f6f2056540faa581c8051a3afc7f4a9f4c169c5
Marc Günther <mguenther@ebay.com>
2017-03-22 17:28:01 +0100

    compile error

--- src/main/java/com/ningen/gomi/drone/java/Main.java
+++ src/main/java/com/ningen/gomi/drone/java/Main.java
@@ -3,7 +3,7 @@ package com.ningen.gomi.drone.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello, world!");
+        Sy stem.out.println("hello, world!");
     }
 
     public static int getOne() {
